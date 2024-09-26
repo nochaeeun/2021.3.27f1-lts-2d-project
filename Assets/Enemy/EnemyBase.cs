b02ec4c202765817b5186f608b2a2ad4a8d6206d
nochaeeun <no3423@naver.com>
2024-09-26 23:43:31 +0900

    EnemyBase_01
    적 베이스 class 작업

--- Assets/Enemy/EnemyBase.cs
+++ Assets/Enemy/EnemyBase.cs
@@ -2,31 +2,70 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using UnityEngine.UIElements;
 public class EnemyBase : MonoBehaviour
 {
 
     // Start is called before the first frame update
-
-    class Enemy
+    public enum EnemyType
     {
-        public string name;
-        public int uneasy;
-        public int stastability;
-        public int attack;
-        public int defense;
-        public int skill;
-        public int rand;
+        Red
+    }
+    public class Enemy
+    {
+        public string Name { get; set; }
+        public int Uneasy { get; set; }
+        public int Stastability { get; set; }
+        public int Attack { get; set; }
+        public int Defense { get; set; }
+        public int Skill { get; set; }
+        public EnemyType Type { get; set; }
 
+        public Enemy(EnemyType type)
+        {
+            Type = type;
+            switch (type)
+            {
+                case EnemyType.Red:
+                    Name = "Red";
+                    Uneasy = 40;
+                    Stastability = 0;
+                    //행동 유형은 여러가지가 있음 공격이 여러 개가 나올 수도 있기 때문에 
+                    //개수대로 할당 필요
+                    break;
+            }
+        }
+
+
+    }
+
+    class EnemycreateRandom
+    {
+        static void main(string[] args)
+        {
+            System.Random random = new System.Random();
+            Array enemtTypes = Enum.GetValues(typeof(EnemyType));
+            //EnemtType의 모든 값을 가져옴
+            EnemyType randomEnemyType = (EnemyType)enemtTypes.GetValue
+                                        (random.Next(enemtTypes.Length));
+            //랜덤으로 하나의 EnemyType 선택
+            Enemy enemy = new Enemy(randomEnemyType); 
+            //랜덤으로 선택된 적 생성
+        }
+    }
+
+    public void Attack()
+    {
+        // 플레이어 코스트와 연결 필요
     }
     void Start()
     {
-        System.Random random = new System.Random();
-
-
     }
     // Update is called once per frame
     void Update()
     {
 
     }
+
+
 }
